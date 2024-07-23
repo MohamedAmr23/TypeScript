c65f894be695e2df4a39e2f5566eb375a2c90388
MohamedAmr23 <amrm96754@gmail.com>
2024-07-23 06:16:10 +0300

    week2

--- src/index.ts
+++ src/index.ts
@@ -208,8 +208,190 @@ console.log(bool)
 //////////////////////////////////////////////////////////////////////
 
 
+// /////////////////////////// lesson 17 ///////////////////////////
+// Data Types - Enums Part 1
+
+/*
+  Data Types
+  - Enums => Enumerations
+  --- Allow Us To Declare A Set Of Named Constants
+  --- Used For Logical Grouping Collection Of Constants "Collection Of Related Values"
+  --- It Organize Your Code
+  --- By Default Enums Are Number-Based, First Element Is 0
+  --- Theres A Numeric Enums
+  --- Theres A String-Based Enums
+  --- Theres Heterogeneous Enums [String + Number]
+*/
+
+// const KIDS = 15;
+// const EASY = 9;
+// const MEDIUM = 6;
+// const HARD = 3;
+
+// enum Level {
+//   Kids = 15,
+//   Easy = 9,
+//   Medium = 6,
+//   Hard = 3
+// }
+
+// let lvl: string = "Easy";
+
+// if (lvl === "Easy") {
+//   console.log(`The Level Is ${lvl} And Number Of Seconds Is ${Level.Easy}`);
+// }
+
+//////////////////////////////////////////////////////////////////////
 
 
+// /////////////////////////// lesson 18 ///////////////////////////
+// Data Types - Enums Part 2
+/*
+  Data Types
+  - Enums => Enumerations
+  --- Enum Can Refer To Other Enum
+  --- Enum Can Refer To Same Enum
+  --- Enum Can Have Calculations
+  --- Enum Can Have Functions
+*/
+
+/*
+  Data Types
+  - Enums => Enumerations
+  --- Enum Can Refer To Other Enum but must be at top of it to can read it
+  --- Enum Can Refer To Same Enum
+  --- Enum Can Have Calculations
+  --- Enum Can Have Functions
+*/
+
+function getHardSeconds() : number {
+    return 3;
+  }
+  
+  enum Kids {
+    Five = 25,
+    Seven = 20,
+    Ten = 15
+  }
+  
+  enum Level {
+    Kid = Kids.Ten,
+    Easy = 9,
+    Medium = Easy - 3,
+    Hard = getHardSeconds()
+  }
+  
+  let lvl: string = "Easy";
+  
+  if (lvl === "Easy") {
+    console.log(`The Level Is ${lvl} And Number Of Seconds Is ${Level.Hard}`);
+  }
+
+
+//////////////////////////////////////////////////////////////////////
+
+
+
+
+
+// /////////////////////////// lesson 19 ///////////////////////////
+// Data Types - Type Assertions
+/*
+  Data Types
+  - Type Assertions
+  --- Sometime Compiler Doesnt Know The Information We Do
+  --- TypeScript Is Not Performing Any Check To Make Sure Type Assertion Is Valid
+*/
+
+// let myImg = document.getElementById("my-img") as HTMLImageElement;
+let myImg = <HTMLImageElement> document.getElementById("my-img");
+console.log(myImg.src);
+
+let data: any = 1000;
+console.log((data as string).repeat(3)); // you should cause error but not give error because TypeScript Is Not Performing Any Check To Make Sure Type Assertion Is Valid
+
+
+//////////////////////////////////////////////////////////////////////
+
+// /////////////////////////// lesson 20 ///////////////////////////
+// Data Types - Union And Intersection Types
+/*
+  Data Types
+  - Union And Intersection Types
+  --- Union Type
+  ------ The | Symbol Is Used To Create The Union => "Or"
+
+  --- Intersection Type
+  ------ Is A Type That Combines Several Types Into One
+  ------ The & Symbol Is Used To Create An Intersection => "And"
+
+  --- If A Union Is An OR, Then An Intersection Is An AND.
+*/
+
+// let all: number | string = 100;
+
+type A = {
+    one: string,
+    two: number,
+    three: boolean
+  }
+  
+  type B = A & {
+    four: number
+  }
+  
+  type C = {
+    five: boolean
+  }
+  
+  type mix = A & C;
+  
+  function getActions(btns: mix) {
+    console.log(`Hello ${btns.one}`);
+    console.log(`Hello ${btns.two}`);
+    console.log(`Hello ${btns.three}`);
+    console.log(`Hello ${btns.five}`);
+  }
+  
+  getActions({ one: "String", two: 100, three: true, five: true });
+
+//////////////////////////////////////////////////////////////////////
+
+
+// /////////////////////////// lesson 21 ///////////////////////////
+// Type Annotations With Object
+/*
+  Type Annotations With Object
+*/
+
+let myObject: {
+    readonly username: string,
+    id: number,
+    hire?: boolean,
+    skills: {
+      one: string,
+      two: string
+    }
+  } = {
+    username: "Elzero",
+    id: 100,
+    hire: true,
+    skills: {
+      one: "HTML",
+      two: "CSS"
+    }
+  };
+  
+  // myObject.username = "Osama";
+  myObject.id = 101;
+  myObject.hire = false;
+  
+  console.log(myObject.username);
+  console.log(myObject.id);
+  console.log(myObject.hire);
+  console.log(myObject.skills.one);
+
+///////////////////////////////////////////////////////////////////
 
 
 
